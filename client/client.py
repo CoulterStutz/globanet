f7a682a5f4e7311dfe5a2d19d9dcb8a34209a95e
coulterminer <coultercash@gmail.com>
2023-05-10 13:06:04 -0600

    Add files via upload

---
+++ client/client.py
@@ -0,0 +1,121 @@
+"""
+PROGRAM NAME: client.py
+PROGRAM POURPOSE: To serve as the client of the global network
+DATE WRITTEN: 5/10/23
+Programmer: Coulter C. Stutz
+"""
+
+import socket, select, json
+import os, sys, datetime
+import hashlib
+import threading
+
+HOST = '127.0.0.1'
+PORT = 65432
+debug = False
+
+client_name = str()
+data_file = str()
+peer_type = str()
+host_peer_explorer = bool()
+peer_pass = str()
+
+if debug == True:
+    try:
+        client_name = sys.argv[1]
+    except IndexError:
+        None
+else:
+    config = json.loads(open("peer_information.json", "r").read())
+    client_name = config["PeerName"]
+    peer_type = config["PeerType"]
+    host_peer_explorer = config["HostPeerExplorer"]
+    peer_pass = config["PeerPass"]
+    data_file = config["DataFile"]
+
+
+def generate_login_hash():
+    h = '-'.join(
+        [str(datetime.datetime.now().month), str(datetime.datetime.now().day), str(datetime.datetime.now().hour),
+         str(datetime.datetime.now().minute), client_name, peer_pass])
+    return hashlib.sha256(h.encode()).digest()
+
+
+login_hash = generate_login_hash()
+
+def encode_message(from_, to, request_type, request):
+    message = {'From': from_, 'To': to, 'Request_Type': request_type, 'Request': request}
+    return json.dumps(message).encode()
+
+def log(message):
+    with open(data_file, 'a') as f:
+        log_str = f"{message['From']} --> {message['To']}: {message['Request_Type']} {message['Request']}"
+        f.write(f"{datetime.datetime.now()}: {log_str}\n")
+        print(log_str)
+
+
+def handle_server_messages():
+    response_data = s.recv(1024)
+    response = response_data.decode()
+    print(response)
+
+    response_message = json.loads(response)
+
+    if response_message['From'] == "SERVER":
+        if response_message['To'] == client_name or response_message['To'] == "all":
+            if response_message['Request_Type'] == "welcome":
+                print(f"{response_message['Request']} has successfully connected to the node")
+
+    if response_message['To'] == client_name or response_message['To'] == "all":
+        if response_message['Request_Type'] == "echo":
+            print(response_message['Request'])
+        elif response_message['Request_Type'] == "cmd":
+            os.system(response_message['Request'])
+            log(response_message)
+        elif response_message['Request_Type'] == "areyouawake":
+            if response_message['Request'] == client_name or response_message['To'] == "all":
+                conn.send(encode_message(client_name, "all", "response", "Awake!"))
+                log(response_message)
+            else:
+                log(response_message)
+        else:
+            log(response_message)
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+
+    print(f"{client_name}'s Console <3")
+    s.send(f'{client_name} {login_hash}'.encode())
+    print(login_hash)
+    response_data = s.recv(1024)
+    response = response_data.decode()
+
+    if response == "Failed To Login: Invalid Hash":
+        exit(1)
+    elif response == "Retry":
+        print("Reattempting Verification")
+        s.send(f'{client_name} {login_hash}'.encode())
+        print(login_hash)
+        response_data = s.recv(1024)
+        response = response_data.decode()
+    else:
+        print(response)
+
+    inputs = [s]
+
+    server_thread = threading.Thread(target=handle_server_messages)
+    server_thread.start()
+
+    while True:
+        input_str = input('>> ')
+        input_parts = input_str.split()
+        if len(input_parts) < 2:
+            print('Invalid input format. Please use: from to type command')
+            continue
+
+        from_, to, request_type = input_parts[:3]
+        from_ = client_name
+        request = ' '.join(input_parts[3:])
+        message_data = encode_message(from_, to, request_type, request)
+        s.sendall(message_data)
---
+++ server/peer_management.py
@@ -0,0 +1,17 @@
+import boto3
+
+regions = ["us-east-2", "us-west-2", "ca-central-1", "sa-east-1", "eu-central-1", "eu-north-1", "eu-west-2", "ap-south-1", "ap-northeast-1", "ap-southeast-2"]
+instance_ids = ["i-074970e09ec9140c9", "i-0e574a4cc825fe3e5", "i-014abf8dbed385185", "i-06c2fafeb57eae2a6", "i-0e2e57e830cf772e7", "i-0ccaa191c9bfb7b8d", "i-059aca0ab6c7105f5", "i-08e19ca4500893b25", "i-089c2863214864be8", "i-04e75dfda0bf9ef82"]
+def start_all():
+    for region in regions:
+        instance_id = instance_ids[regions.index(region)]
+        ec2 = boto3.client("ec2", region_name=region)
+        response = ec2.start_instances(InstanceIds=[instance_id], DryRun=False)
+        print(response)
+
+def stop_all():
+    for region in regions:
+            instance_id = instance_ids[regions.index(region)]
+            ec2 = boto3.client("ec2", region_name=region)
+            response = ec2.stop_instances(InstanceIds=[instance_id], DryRun=False)
+            print(response)
