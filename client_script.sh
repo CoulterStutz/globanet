79ae6dbbf617f7576ee7dc669d4681395d9d42c0
coulterminer <coultercash@gmail.com>
2023-05-17 07:43:45 -0600

    Add files via upload

---
+++ client_script.sh
@@ -0,0 +1,3 @@
+curl "https://raw.githubusercontent.com/coulterminer/globanet/main/client/client.py" >> client.py
+curl "https://raw.githubusercontent.com/coulterminer/globanet/main/client/references.py" >> refrences.py
+python3 client.py
